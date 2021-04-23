22755574d8faefded668facf694b0077bb04930d
Muskaan Shraogi <54302920+muskaanshraogi@users.noreply.github.com>
2021-04-23 22:40:05 +0530

    Add files via upload

---
+++ GFG/Linked List/nthNodeFromEndOfLinkedList.cpp
@@ -0,0 +1,89 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(Node *head, int n)
+{
+       // Your code here
+       int i;
+       Node *p1 = head, *p2 = head;
+       
+       for(i=1; i<n && p1->next; i++)
+       {
+           p1 = p1->next;
+       }
+       
+     
+       if(i == n)
+       {
+           while(p1 && p1->next)
+           {
+               p1 = p1->next;
+               p2 = p2->next;
+           }
+           
+           return p2->data;
+       }
+       
+       return -1;
+}
+
+
